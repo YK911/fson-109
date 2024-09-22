8430a3383db8e4842481ed4431b1a4a1a0a74282
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-09-22 23:27:48 +0300

    changes

--- module-11/lesson-01/js/03-practice.js
+++ module-11/lesson-01/js/03-practice.js
@@ -1,3 +1,61 @@
 /**
  * Використовуємо https://pokeapi.co/ та створимо сторінку перегляду покемонів
  */
+import { BASE_POKEMON_URL } from './config.js';
+
+const refs = {
+  form: document.querySelector('.js-search-form'),
+  container: document.querySelector('.js-container'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+
+function fetchPokemon(searchQuery = '') {
+  return fetch(`${BASE_POKEMON_URL}/pokemon/${searchQuery}`).then(response => {
+    if (!response.ok) {
+      throw new Error('Упс... щось пішло не так, спробуйте ще раз');
+    }
+
+    return response.json();
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const { query } = form.elements;
+
+  fetchPokemon(query.value)
+    .then(data => {
+      const pokemonMarkup = createMarkup(data);
+      refs.container.innerHTML = pokemonMarkup;
+    })
+    .catch(error => {
+      iziToast.error({
+        title: '❌',
+        message: error.message,
+        position: 'topRight',
+      });
+    });
+}
+
+function createMarkup({ abilities, name, weight, height, sprites }) {
+  const abilityListItems = abilities
+    .map(({ ability: { name } }) => `<li>${name}</li>`)
+    .join('');
+
+  return `<article class="card">
+        <div class="card-img-top">
+          <img src="${sprites?.other?.home?.front_default}" alt="${name}" />
+        </div>
+        <div class="card-body">
+          <h2 class="card-title">Ім'я: ${name}</h2>
+          <p class="card-text">Вага: ${weight}</p>
+          <p class="card-text">Зростання: ${height}</p>
+
+          <p class="card-text"><b>Уміння</b></p>
+          <ul class="list-group">
+            ${abilityListItems}
+          </ul>
+        </div>
+      </article>`;
+}
--- module-11/lesson-01/js/config.js
+++ module-11/lesson-01/js/config.js
@@ -1,2 +1,4 @@
 export const BASE_URL = 'https://pixabay.com/api/';
 export const API_KEY = '15249615-5ccf49bef51d4f01888f64cb2';
+
+export const BASE_POKEMON_URL = 'https://pokeapi.co/api/v2';
